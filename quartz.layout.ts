22e5085816878ac6616cca2a8c27bbaed17236ba
Celestial-Entity <139852159+stardoom4@users.noreply.github.com>
2024-04-04 21:39:57 +0530

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,8 +8,6 @@ export const sharedPageComponents: SharedLayout = {
   footer: Component.Footer({
     links: {
       GitHub: "https://github.com/stardoom4", 
-      Mastodon:
-"https://defcon.social/@zarathustra",
     },
   }),
 }

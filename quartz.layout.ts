d3fb99a6a2ab4a16dd52bdcc0964d7d8633d809b
Celestial-Entity <139852159+stardoom4@users.noreply.github.com>
2024-04-06 00:15:40 +0530

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -9,7 +9,7 @@ export const sharedPageComponents: SharedLayout = {
     links: {
       GitHub: "https://github.com/stardoom4", 
       Bookmarks: "https://raindrop.io/Celestial-Entity/main-43067410",
-      Revered-Mother: "https://garud.netlify.app/Bharat-Mata",
+      "Revered-Mother": "https://garud.netlify.app/Bharat-Mata",
     },
   }),
 }
